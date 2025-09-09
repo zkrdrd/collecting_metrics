4abd85fb869eda1b754e3cf2b66d7537574fb6a3
delphin <eduard.zakirov.96@mail.ru>
2025-09-09 22:59:54 +0500

    add coments and metric atributes

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	// NewMeterProvider returns a new and configured MeterProvider.
 	// By default, the returned MeterProvider is configured with the default Resource and no Readers.
 	// Readers cannot be added after a MeterProvider is created.
@@ -32,19 +33,34 @@ func main() {
 
 	// WithReader associates Reader r with a MeterProvider.
 	// By default, if this option is not used, the MeterProvider will perform no operations; no data will be exported without a Reader.
+
+	// Meter returns a Meter with the given name and configured with options.
+	// The name should be the name of the instrumentation scope creating telemetry.
+	// This name may be the same as the instrumented code only if that code provides built-in instrumentation.
+	// Calls to the Meter method after Shutdown has been called will return Meters that perform no operations.
+	// This method is safe to call concurrently.
 	meterProvider := metric.NewMeterProvider(metric.WithReader(exporter)).Meter(meterName)
 
 	go server()
 
+	// WithAttributes converts attributes into an attribute Set and sets the Set to be associated with a measurement.
 	measurementOption := otelMetric.WithAttributes(
 		attribute.Key("A").String("B"),
 		attribute.Key("C").String("D"),
 	)
 
+	// Float64Counter is an instrument that records increasing float64 values.
+	// Warning: Methods may be added to this interface in minor releases.
+	// See package documentation on API implementation for information on how to set default behavior for unimplemented methods.
+
+	// WithDescription sets the instrument description.
 	float64Counter, err := meterProvider.Float64Counter("foo", otelMetric.WithDescription("foo description"))
 	if err != nil {
 		log.Fatal(err)
 	}
+	// Add records a change to the counter.
+	// Use the WithAttributeSet (or, if performance is not a concern,
+	// the WithAttributes) option to include measurement attributes.
 	float64Counter.Add(ctx, 5, measurementOption)
 }
 
